9b952d9d997bd2acc86c56a9978af8f3c3ef983e
MrJackdaw <MrJackdaw@users.noreply.github.com>
2024-06-10 16:35:35 -0700

    * split-file will emit zip segments when they hit 1GB threshold

--- public/workers/Workers.SplitFile.mjs
+++ public/workers/Workers.SplitFile.mjs
@@ -2,7 +2,10 @@
 import JSZip from "jszip";
 import { exportWorkerAlert, exportWorkerFile } from "./Workers.State.mjs";
 
-/* ITERATION 3x */
+/** Convert a number to Megabytes */
+const megabytesToBytes = (n) => n * 1048576;
+const bytesToMegabytes = (n) => n / 1048576;
+const GIGAB = megabytesToBytes(1000);
 
 /**
  * Split file
@@ -11,33 +14,51 @@ import { exportWorkerAlert, exportWorkerFile } from "./Workers.State.mjs";
  */
 export async function splitTextFile(file, numSegments = 5) {
   const totalSize = file.size;
-  // Dynamically calculate chunk size based on file size
-  const chunkSize = Math.min(Math.ceil(totalSize / numSegments), 209715200);
-  const zip = new JSZip();
+  // Restrict max chunk size to 200MB
+  const chunkSize = Math.min(
+    Math.ceil(totalSize / numSegments),
+    megabytesToBytes(200)
+  );
+
+  const expectSegments = Math.ceil(totalSize / chunkSize);
   const reader = file.stream().getReader();
   let currentSegmentIndex = 0;
   let currentSegmentContent = "";
+  let zip = new JSZip();
 
   // Overwrite encoding
   const textDecoder = new TextDecoder("utf-8", { fatal: true });
   let addedSegments = 0;
+  let accumulatedSize = 0;
 
   // Helper function to add files to zip
-  function addSegmentToFile(segmentContent, index) {
+  async function addSegmentToFile(segmentContent, index) {
     const blob = new Blob([segmentContent], { type: "text/plain" });
+    const blobSizeMB = Math.round(bytesToMegabytes(blob.size));
     const fileName = `${file.name}_part_${index + 1}.txt`;
     zip.file(fileName, blob);
     addedSegments = addedSegments + 1;
-    exportWorkerAlert(
-      `Created: ${fileName} (size ${blob.size} bytes)`,
-      "Info",
-      true
-    );
-    blob.text().then(() => URL.revokeObjectURL(blob)); // Cleanup memory
+    accumulatedSize += megabytesToBytes(blobSizeMB);
+
+    URL.revokeObjectURL(blob); // Cleanup memory
+
+    const progMsg = `Created ${fileName} (~${blobSizeMB} MB)...`;
+    exportWorkerAlert(progMsg, "Info", true);
   }
 
   let inProgress = true;
   let hasError = false;
+  let emittedZipFiles = 0;
+
+  // Emit a zip file with all accumulated segments so far
+  async function emitZipSegment() {
+    emittedZipFiles += 1;
+    const zipFileName = `${file.name}_segments-${emittedZipFiles}.zip`;
+    const info = `Created ${addedSegments} of ${expectSegments} segments`;
+    const zipped = await zip.generateAsync({ type: "blob" });
+    exportWorkerFile(zipped, zipFileName);
+    exportWorkerAlert(info);
+  }
 
   while (inProgress) {
     try {
@@ -57,6 +78,20 @@ export async function splitTextFile(file, numSegments = 5) {
         currentSegmentIndex++;
         currentSegmentContent = ""; // Reset for the next segment
       }
+
+      // When accumulated zip is larger than 1GB, warn user, emit a file, and then continue.
+      if (accumulatedSize >= GIGAB) {
+        let warn = `Your file download is at or over 1GB.`;
+        warn = `${warn} Generating zip file with ${addedSegments} of ${expectSegments} segments:`;
+        warn = `${warn} please don't refresh the page until all segments are completed.`;
+        exportWorkerAlert(warn, "Warning");
+
+        await emitZipSegment();
+
+        // reset zip context
+        zip = new JSZip();
+        accumulatedSize = 0;
+      }
     } catch (error) {
       console.error("Error processing file:", error);
       hasError = true;
@@ -69,8 +104,5 @@ export async function splitTextFile(file, numSegments = 5) {
     return exportWorkerAlert(`Error: ${file.name} was not split`, "Error");
   }
 
-  const zipped = await zip.generateAsync({ type: "blob" });
-  const zipFileName = `${file.name}_segments.zip`;
-  exportWorkerFile(zipped, zipFileName);
-  exportWorkerAlert(`${file.name} split into ${addedSegments} segments`);
+  emitZipSegment();
 }
