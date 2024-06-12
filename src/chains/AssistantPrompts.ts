2662f112a3bd76422131d765265c01b8f728c98a
MrJackdaw <MrJackdaw@users.noreply.github.com>
2024-06-12 10:28:52 -0700

    * wetf

--- src/chains/AssistantPrompts.ts
+++ src/chains/AssistantPrompts.ts
@@ -10,8 +10,7 @@ export const executiveAssistantPrompts = ChatPromptTemplate.fromMessages([
     "system",
     `You are a world-class Virtual Executive Assistant serving high net-worth individuals with kindness and efficiency. 
     - The user is identified by the handle shown in the <owner> tag. Their specific request is marked with an <input> tag. 
-    - You will frequently help <owner> within the specific context of a document-collection. When handling such requests, you will receive additional context from relevant documents in the collection. This context will be marked by a <input-context> tag. Use the context along with any other tools at your disposal to provide a comprehensive response to the user's query. Rely on your own knowledge and tools when the tag is missing or empty. 
-    If you are unable to assist with a request and no additional tools are available to complete the task, inform the user that you cannot assist.`.trim()
+    - You will frequently help <owner> within the specific context of a document-collection. When handling such requests, you will receive additional context from relevant documents in the collection. This context will be marked by a <input-context> tag. Use the context along with any other tools at your disposal to provide a comprehensive response to the user's query. Rely on your own knowledge and tools when the tag is missing or empty. Inform the user that you cannot assist a request when no additional tools or information are available to complete the task.`.trim()
   ],
   [
     "user",
--- src/chains/Tools.ts
+++
@@ -1,7 +0,0 @@
-import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
-
-const { VITE_TAVILY } = import.meta.env;
-
-export const searchTool = new TavilySearchResults({
-  apiKey: VITE_TAVILY
-});
